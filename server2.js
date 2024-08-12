c2bcdfe9654af0fafa1c2d2534b2322b1e2841b9
PVSKR <skr.polu@hotmail.com>
2024-08-12 22:49:57 +0530

    server2.js file created
    Local host:4000 used

--- server2.js
+++ server2.js
@@ -45,6 +45,6 @@ function authenticateToken(req, res, next) {
   });
 }
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(4000, () => {
+  console.log("Server running on port 4000");
 });
